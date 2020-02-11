c37e8ed47e0860d0eed209ed8269a0504944f78a
Aliaksandra Neviarouskaya <Aliaksandra.Neviarouskaya@ibm.com>
2020-02-11 18:04:52 +0300

    Add Two Sum task

---
+++ two_sum/test_solution.py
@@ -0,0 +1,13 @@
+import pytest
+
+from two_sum import Solution
+
+
+@pytest.fixture(scope='session', autouse=True)
+def init_solution():
+    solution = Solution()
+    return solution
+
+
+def test_twoSum(init_solution):
+    assert init_solution.twoSum([2, 7, 11, 15], 9) == (0, 1)
